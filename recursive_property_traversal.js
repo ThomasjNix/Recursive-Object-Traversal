fde1c3b16367c62979903b4f5e5b389c1b7992fc
Thomas Nix <tnix2@uncc.edu>
2018-08-23 23:47:42 -0400

    Add file example

---
+++ recursive_property_traversal.js
@@ -0,0 +1,47 @@
+// Define generic multilevel JSON object
+const JSONObj = {
+	"data1": {
+		"key1": "value1",
+		"key2": "value2"
+	},
+	"data2": {
+		"key3": "value3",
+		"key4": "value4"
+	}
+}
+
+// stringify JSON object
+const stringInterp = JSON.stringify(JSONObj);
+
+// parse string interpretation of JSON Object
+const newObject = JSON.parse(stringInterp);
+
+// get own properties of object
+const keyArr = Object.keys(newObject);
+
+/**
+* Recursive function to print all node level properties of the object
+* param obj - object to assess
+* param key - property of object being assessed
+*/
+const printAllVals = function(obj, prop) {
+	if (typeof obj[prop] == 'object') {
+		Object.keys(obj[prop]).forEach((key) => {
+			printValues(obj[prop], key);
+		})
+	} else { 
+		console.log(obj[prop]);
+	}
+}
+
+// Iterate over first branch of properties, call printAllVals
+// recursive function with each branch and entire object as object to assess
+newObject.forEach((key) => {
+    printAllVals(newObject, key);
+});
+
+// value1
+// value2
+// value3
+// value4
+// undefined (execution complete; no return value)
